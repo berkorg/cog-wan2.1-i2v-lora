3f6b8349c3e06b9722a7890201f76154f4dab4cf
Berk Elmas <berkelmas96@gmail.com>
2025-03-29 01:05:50 +0300

    fix image url

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ predictor = Predictor()
 predictor.setup()
 
 predictor.predict(
-    image="blob:https://replicate.com/39779b31-fdb1-4ded-9302-25a3313efda7",
+    image="https://saas-blogs-bucket.s3.us-east-1.amazonaws.com/d822c90b-52e4-4202-998f-f8e433d2335c.webp",
     prompt="The video begins with a photo of a person. Then the 8r1d3 bride effect occurs. The person is now in a white wedding dress, holding a bouquet.",
     negative_prompt="low quality, bad quality, blurry, pixelated, watermark",
     lora_url="https://huggingface.co/berkelmas/wan-video-loras/resolve/main/bride_it_wan_lora.safetensors",
