6dea08888a7edc6ce66b3610c1881d5dfd0ea2e5
Berk Elmas <berkelmas96@gmail.com>
2025-03-30 17:29:04 +0300

    update test file

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ predictor = Predictor()
 predictor.setup()
 
 predictor.predict(
-    image="https://saas-blogs-bucket.s3.us-east-1.amazonaws.com/d822c90b-52e4-4202-998f-f8e433d2335c.webp",
-    prompt="The video begins with a photo of a person. Then the 8r1d3 bride effect occurs. The person is now in a white wedding dress, holding a bouquet.",
+    image="https://saas-blogs-bucket.s3.us-east-1.amazonaws.com/wan_input_images/cdb9c61d-a29a-4482-9ba1-6e0f1ae1dedf.webp",
+    prompt="The video begins with a photo of a person. then t2k1s takes off clothes revealing a lean muscular body and shows off muscles, looking towards the camera",
     negative_prompt="low quality, bad quality, blurry, pixelated, watermark",
-    lora_url="https://huggingface.co/berkelmas/wan-video-loras/resolve/main/bride_it_wan_lora.safetensors",
+    lora_url="https://huggingface.co/berkelmas/wan-video-loras/resolve/main/muscle_show_off_wan_lora.safetensors",
     lora_strength=1,
     duration=3,
     fps=16,
